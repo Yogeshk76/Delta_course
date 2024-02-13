705d2b4ec73841491f5d8664e5e4d510308b47ad
Yogeshk76 <yogeshkumar60508@gmail.com>
2024-02-13 22:30:41 +0530

    update

---
+++ Delta_javaScript/day1.js
@@ -0,0 +1,62 @@
+// Using the console(REPL: read-evaluate-print-loop)
+// variable: name of a storage location 
+
+// Data Types in JS:(primitive)
+// number/boolean/string/undefined/null/bigint/symbol 
+
+// Number: positive/negative/integer/floating-numbers/
+// Operation: addition/subtraction/mulitiplication/division/modulo/exponentiation
+// modulo: to detect even or odd number 
+
+// NaN
+
+// Operator Precendence (priority)
+// () / ** / 8,/,%  /  +,- 
+
+
+// assignment operators :
+// age = age + 1
+// age += 1
+
+// age = age - 1
+// age -= 1
+
+// age = age * 1
+// age *= 1
+
+
+// unary operators :
+// age = age + 1            age = age - 1      
+// age += 1                 age -= 1
+// age++// increment        age--// decrement
+
+// pre-increment ++age (change,then use)
+// post-increment age++ (use, then change)
+
+// Identifier Rules:
+// names can contain letters,digits,underscore,and dollar signs.(no space)
+// names must begin with a letter
+// names can also begin with $ and_
+// names are case sensitive
+// reserved words like JavaScript keywords cannot be used as names
+
+// camelCase(way of writing identifiers):
+// camelCase(JS naming convention)
+// snake_case
+// PascalCase
+
+
+// we can change datatype in js by simply changing its value 
+
+// typescript=static typed;(designed by microsoft)
+// js=dynamic typed;
+
+// strings are text or sequence of characters
+// space are included
+// to print something with quotes use single quote '' outside and vice versa
+
+// String Indices
+// concatenation: joining multiple strings 
+// "tony"+""+"stark" = "tony stark"
+
+// null & undefined 
